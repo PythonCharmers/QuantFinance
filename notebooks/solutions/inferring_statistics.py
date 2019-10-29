e58920260e07dc9a71fbf971229235fe78300bcc
Ed Schofield <ed@pythoncharmers.com>
2019-10-29 18:37:02 +1100

    Fix typos and spelling mistakes

--- notebooks/solutions/inferring_statistics.py
+++ notebooks/solutions/inferring_statistics.py
@@ -1,5 +1,5 @@
 
-# 1. We say we reject the null hypothesis, as it has a p value of less than 0.05, i.e. a less than 5% chance of occuring purely by chance.
+# 1. We say we reject the null hypothesis, as it has a p value of less than 0.05, i.e. a less than 5% chance of occurring purely by chance.
 # 2. The likelihood of the samples given the assumptions is 0.08% (i.e. less than a tenth of 1%).
 # 3. No. We are confident the expected value is more than 3.5, but we do not know why (it may be loaded to 5s for instance).
 
