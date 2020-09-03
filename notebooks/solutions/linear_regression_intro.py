0c71303b10fe72f7eb57ce445449e9d789e6f81a
Robert Layton <robertlayton@gmail.com>
2020-09-03 12:45:55 +1000

    Update 1.3.1 to use financial data rather than study hours

--- notebooks/solutions/linear_regression_intro.py
+++ notebooks/solutions/linear_regression_intro.py
@@ -1,24 +1,24 @@
 
 # Question 1
 # You wouldn't use the actual error, as the "negative" errors would cancel out the positive errors
-raw_error = np.sum(predicted_scores - SAT_scores)
+raw_error = np.sum(predicted_prices - prices['MedianPrice'])
 print(raw_error)  # note how "small" this is, compared to the absolute error
 
 
 # Question 2
 # (beta, intercept)
-models = [(30, 300),
-          (20, 250),
-          (40, 400)]
+models = [(-100, 1100),
+          (-130, 1200),
+          (-90, 900)]
 
 best_error = None
 best_model = None
 
 for model in models:
     print("Model is: {}".format(model))
-    b, c = model
-    predicted_scores = study_hours * b + c  # Same values as before - this is our "model"
-    squared_error = np.sum((predicted_scores - SAT_scores) ** 2)
+    m, c = model
+    predicted_scores = prices['CashRate'] * m + c  # Same values as before - this is our "model"
+    squared_error = np.sum((predicted_prices - prices['MedianPrice']) ** 2)
     print(" The squared error is {:.2f}".format(squared_error))
     
     if best_error is None or squared_error < best_error:
--- notebooks/solutions/ols_basic_ensemble.py
+++ notebooks/solutions/ols_basic_ensemble.py
@@ -1,50 +1,49 @@
 # Question 1
-mask = study_hours < 20
-study_hours_cleaned = study_hours[mask]
-SAT_scores_cleaned = SAT_scores[mask]  # Don't forget to use the same mask for both X and Y
+mask = cash_rate < 6
+cash_rate_cleaned = cash_rate[mask]
+house_prices_cleaned = house_prices[mask]  # Don't forget to use the same mask for both X and Y
 
-plt.plot(study_hours_cleaned, SAT_scores_cleaned, 'ro')
-plt.xlabel("Hours Studied")
+plt.plot(cash_rate_cleaned, house_prices_cleaned, 'ro')
+plt.xlabel("Cash Rate")
 plt.ylabel("SAT Score")
 
-ones_cleaned = np.ones((len(study_hours_cleaned)))
-X_cleaned = np.vstack([ones_cleaned, study_hours_cleaned]).T
+ones_cleaned = np.ones((len(cash_rate_cleaned)))
+X_cleaned = np.vstack([ones_cleaned, cash_rate_cleaned]).T
 
-Y_cleaned = SAT_scores_cleaned.reshape((len(SAT_scores_cleaned), 1))
+Y_cleaned = house_prices_cleaned.reshape((len(house_prices_cleaned), 1))
 
 beta_cleaned = np.linalg.inv(X_cleaned.T.dot(X_cleaned)).dot(X_cleaned.T.dot(Y_cleaned))
 print(beta_cleaned)
 
-plt.plot(study_hours_cleaned, SAT_scores_cleaned, 'ro')
-plt.xlabel("Hours Studied")
-plt.ylabel("SAT Score")
+plt.plot(cash_rate_cleaned, house_prices_cleaned, 'ro')
+plt.xlabel("Cash Rate")
+plt.ylabel("House Prices")
 
 # Here we create x values for "each hour of study" so we can plot the results
-x_model = np.arange(0, 25)
+x_model = np.arange(0, 8, 0.25)
 y_model = x_model * beta[1] + beta[0]  # Use our learned model to fit the line of best fit.
 
 plt.plot(x_model, y_model, 'b-')
-
 
 # Question 2
 
 # Store parameter versions here
 models = []
 
-n = len(study_hours)
+n = len(cash_rate_cleaned)
 
 for i in range(100):
     
     # Choose random indexes
     # replace=True is better statistically, we will come back to it later on...
     random_indexes = np.random.choice(np.arange(n), size=int(n/2), replace=True)
-    study_hours_sample = study_hours[random_indexes]
-    SAT_scores_sample = SAT_scores[random_indexes]
+    cash_rate_sample = cash_rate[random_indexes]
+    house_prices_sample = house_prices[random_indexes]
     
-    ones_sample = np.ones((len(study_hours_sample)))
-    X_sample = np.vstack([ones_sample, study_hours_sample]).T
+    ones_sample = np.ones((len(cash_rate_sample)))
+    X_sample = np.vstack([ones_sample, cash_rate_sample]).T
 
-    Y_sample = SAT_scores_sample.reshape((len(SAT_scores_sample), 1))
+    Y_sample = house_prices_sample.reshape((len(house_prices_sample), 1))
 
     beta_sample = np.linalg.inv(X_sample.T.dot(X_sample)).dot(X_sample.T.dot(Y_sample))
     models.append(beta_sample)
