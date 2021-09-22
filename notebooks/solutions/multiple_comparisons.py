65c7d453e4653825511e6456e5f82113fba9b2db
Hixan <alex.88.elias@gmail.com>
2021-09-22 12:07:48 +1000

    More typos, and formatting.
    numerous typos, eg:
    - missing closing brace in 2.4.1, and cells appeared in the wrong
    order (solutions file was after another cell)
    - incorrect name of solution for 2.5.1
    
    rename why_not_normal to why_not_straight as the name of the file
    gives away the answer to the exersise

--- notebooks/solutions/multiple_comparisons.py
+++ notebooks/solutions/multiple_comparisons.py
@@ -1,2 +1,3 @@
 
-# After modify the threshold, the result is no longer significant.
+# see FWER (https://en.wikipedia.org/wiki/Family-wise_error_rate) to adjust the significance threshold
+# After modifying the threshold, the result is no longer significant.
