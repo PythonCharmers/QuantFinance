151a44d4e84e74f01a6939f19e957329659d6b5d
alireza darvishi <alidarvishi14@gmail.com>
2022-02-19 04:23:15 +0330

    Update ljungbox.py

--- notebooks/solutions/ljungbox.py
+++ notebooks/solutions/ljungbox.py
@@ -1,3 +1,6 @@
 
-# We reject that the data is independently distributed for lags of 1 and 2, giving evidence for H_A
+# We can not reject that the data is independently distributed for lags of 1 and 2, not giving evidence for H_A
+# that there is a serial correlation for these two lag periods.
+
+# We can reject that the data is independently distributed for lags of 3 and more, giving evidence for H_A
 # that there is a serial correlation for these two lag periods.
