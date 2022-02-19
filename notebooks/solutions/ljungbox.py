e2d2e697d78ddcb2607982811b7abd422ec01003
alireza darvishi <alidarvishi14@gmail.com>
2022-02-19 04:23:53 +0330

    Update ljungbox.py

--- notebooks/solutions/ljungbox.py
+++ notebooks/solutions/ljungbox.py
@@ -3,4 +3,4 @@
 # that there is a serial correlation for these two lag periods.
 
 # We can reject that the data is independently distributed for lags of 3 and more, giving evidence for H_A
-# that there is a serial correlation for these two lag periods.
+# that there is a serial correlation for these lag periods.
